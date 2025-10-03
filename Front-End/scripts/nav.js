e7a96f8218d09d1966ae1b70b69061025f39955e
Rian Aguiar Santos <aguiarsantosrian@gmail.com>
2025-10-02 22:51:31 -0300

    difinitivi edition

--- Front-End/scripts/nav.js
+++ Front-End/scripts/nav.js
@@ -5,30 +5,30 @@ document.addEventListener("DOMContentLoaded", function () {
     if (navElement) {
         navElement.innerHTML = `
 
-            <div class="nav cl">
-                <div class="nav-content rw">
+  <div class="nav cl">
+    <div class="nav-content rw">
+      <div class="nav-left rw">
 
-                <div class="nav-left rw">
+        <img src="arquivos/imagens/logo.png" alt="logo" class="nav-logo">
+        <div class="nav-options rw">
+          <a href="alunos.html"><span class="icon-nav users"></span></a>
+          <a href="documentos.html"><span class="icon-nav file-text"></span></a>
+          <a href="modelos.html"><span class="icon-nav square-pencil"></span></a>
+        </div>
 
-                    <img src="arquivos/imagens/logo.png" alt="logo" class="nav-logo">
-                    <div class="nav-options rw">
-                    <a href="alunos.html"><span class="icon-nav users"></span></a>
-                    <a href="documentos.html"><span class="icon-nav file-text"></span></a>
-                    <a href="modelos.html"><span class="icon-nav square-pencil"></span></a>
-                    </div>
-
-                </div>
-                <a href="perfil.html">
-                <div class="nav-perfil rw">
-                    <div class="cl">
-                        <p class="name">Carlos Roberto Pinheiro</p>
-                        <p class="email">Carlos.pinheiro8@fatec.sp.gov.br</p>
-                    </div>
-                    <img src="arquivos/imagens/ft-perfil.png" alt="foto de perfil" class="foto-perfil">
-                    </div>
-                </a>
-                </div>
-            </div>
+      </div>
+      <a href="perfil.html">
+        <div class="nav-perfil rw">
+          <div class="cl">
+            <p class="name">Carlos Roberto Pinheiro</p>
+            <p class="email">Carlos.pinheiro8@fatec.sp.gov.br</p>
+          </div>
+          <img src="arquivos/imagens/silvia.png" alt="foto de perfil" class="foto-perfil">
+          <img src="arquivos/icons/teacher.png" style="width: 42px;">
+        </div>
+      </a>
+    </div>
+  </div>
         
         `;
     } 
---
+++ Front-End/scripts/navAluno.js
@@ -0,0 +1,39 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navElement = document.querySelector('nav');
+
+    if (navElement) {
+        navElement.innerHTML = `
+
+  <div class="nav cl">
+    <div class="nav-content rw">
+      <div class="nav-left rw">
+
+        <img src="arquivos/imagens/logo.png" alt="logo" class="nav-logo">
+        <div class="nav-options rw">
+          <a href="area_aluno.html"><span class="icon-nav users"></span></a>
+          <a href="enviados.html"><span class="icon-nav file-text"></span></a>
+        </div>
+
+      </div>
+      <a href="perfilAluno.html">
+        <div class="nav-perfil rw">
+          <div class="cl">
+            <p class="name">Carlos Roberto Pinheiro</p>
+            <p class="email">Carlos.pinheiro8@fatec.sp.gov.br</p>
+          </div>
+          <img src="arquivos/imagens/user.jpg" alt="foto de perfil" class="foto-perfil">
+          <img src="arquivos/icons/graduation-cap.png" alt="">
+        </div>
+      </a>
+    </div>
+  </div>
+        
+        `;
+    } 
+    
+    else {
+        console.log('A tag <nav> NÃO foi encontrada no documento.');
+    }
+
+})
