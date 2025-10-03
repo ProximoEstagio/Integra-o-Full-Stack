c11f83d5d29bdd8d8bc2cd9ac6a6d2d57c2bdd2c
Rian Aguiar Santos <aguiarsantosrian@gmail.com>
2025-10-02 23:59:04 -0300

    swito

--- Front-End/scripts/nav.js
+++ Front-End/scripts/nav.js
@@ -8,25 +8,30 @@ document.addEventListener("DOMContentLoaded", function () {
   <div class="nav cl">
     <div class="nav-content rw">
       <div class="nav-left rw">
-
         <img src="arquivos/imagens/logo.png" alt="logo" class="nav-logo">
         <div class="nav-options rw">
           <a href="alunos.html"><span class="icon-nav users"></span></a>
           <a href="documentos.html"><span class="icon-nav file-text"></span></a>
           <a href="modelos.html"><span class="icon-nav square-pencil"></span></a>
         </div>
-
       </div>
-      <a href="perfil.html">
-        <div class="nav-perfil rw">
-          <div class="cl">
-            <p class="name">Silvia Maria Farani Costa</p>
-            <p class="email">Silvia.Costa8@fatec.sp.gov.br</p>
+
+      <div class="wrapper">
+        <a href="perfilAluno.html">
+          <div class="nav-perfil rw">
+            <div class="cl">
+              <p class="name">Silvia Maria Farani Costa</p>
+              <p class="email">Silvia.Costa8@fatec.sp.gov.br</p>
+            </div>
+            <img src="arquivos/imagens/silvia.png" alt="foto de perfil" class="foto-perfil">
+            <img src="arquivos/icons/teacher.png" style="width: 42px">
           </div>
-          <img src="arquivos/imagens/silvia.png" alt="foto de perfil" class="foto-perfil">
-          <img src="arquivos/icons/teacher.png" style="width: 42px;">
-        </div>
-      </a>
+        </a>
+        <label class="switch rw">
+          <input type="checkbox" checked>
+          <span class="slider round"></span>
+        </label>
+      </div>
     </div>
   </div>
         
